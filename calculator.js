db50e26445679a9f483c54ad002105d996a1260a
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-01-09 22:51:33 +0600

    post request added

--- calculator.js
+++ calculator.js
@@ -1,11 +1,24 @@
 //jshint esversion6
 
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/",function(request, respond){
     respond.sendFile(__dirname + "/index.html")
 });
+
+app.post("/", function(request, respond){
+
+    var num1 = Number(request.body.num1);
+    var num2 = Number(request.body.num2);
+
+    var result = num1 + num2;
+
+    respond.send("Result = " + result);
+})
 
 app.listen(3000,function(){
     console.log("Server started at port 3000");
