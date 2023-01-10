0960e4210dff440cbee242f28489e75ee4b1ad14
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-01-10 22:27:23 +0600

    BMI calculator added

--- calculator.js
+++ calculator.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/",function(request, respond){
-    respond.sendFile(__dirname + "/index.html")
+    respond.sendFile(__dirname + "/bmiCalculator.html")
 });
 
 app.post("/", function(request, respond){
@@ -15,9 +15,11 @@ app.post("/", function(request, respond){
     var num1 = Number(request.body.num1);
     var num2 = Number(request.body.num2);
 
-    var result = num1 + num2;
+    var result = num1 / (num2*num2);
 
-    respond.send("Result = " + result);
+    let result2 = result.toFixed(2);
+
+    respond.send("Your BMI is " + result2);
 })
 
 app.listen(3000,function(){
